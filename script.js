4690ba8b061b6470e69487b9a6abda66ddd7d2d3
sayyidal1596 <sayyid.al1596@smk.belajar.id>
2026-02-10 17:01:34 +0700

    update

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener('mousemove', (e) => {
 });
 
 // Typing effect for greeting
-const greetingText = "Hey You Know What! You're the most adorable human i ever met! 💖";
+const greetingText = "Haii, selamat ulang tahun yaa, aku ada sedikit ucapan for maneh 💖";
 const greetingElement = document.querySelector('.greeting');
 let charIndex = 0;
 
